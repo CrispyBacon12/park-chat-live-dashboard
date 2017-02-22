2e0aa16ada774821f152cbe50102ff64bc50888f
Chris Purssell <chris.purssell@gmail.com>
2017-02-22 16:48:20 +1000

    Increase size and reduce whitespace of comment stream.
    For max visibility at distance on TV.

--- src/components/readonly-comments-list-item.js
+++ src/components/readonly-comments-list-item.js
@@ -7,9 +7,9 @@ export default class ReadOnlyCommentsListItem extends Component {
 
     return (
       <li className="list-group-item">
-        <div className="d-flex w-100 justify-content-between">
-          <p className="mb-1 text-primary"><small>{comment.from.name}</small></p>
-          <small><TimeAgo date={comment.created_time} /></small>
+        <div className="d-flex w-100 justify-content-between comment-heading mb-3">
+          <p className="text-primary"><small>{comment.from.name}</small></p>
+          <p className="text-muted"><small><TimeAgo date={comment.created_time} /></small></p>
         </div>
         <p className="mb-1">{comment.message}</p>
       </li>
