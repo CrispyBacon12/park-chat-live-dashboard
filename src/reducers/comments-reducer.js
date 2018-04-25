bc96c1da7e839835802a3c587adf3cc0518b3ba8
Chris Purssell <chris.purssell@gmail.com>
2018-04-25 12:40:19 +1000

    Add avengers 3 filter

--- src/reducers/comments-reducer.js
+++ src/reducers/comments-reducer.js
@@ -9,6 +9,10 @@ export function CommentsReducer(state = [], action) {
 }
 
 function addComments(state, action) {
+  if (containsBlackListedWord(action.message)) {
+    action.message = 'This message has triggered the filter.';
+  }
+
   // we want to merge with the existing state, excluding any comments that are being overriden
   // by this new payload, to avoid duplicates.
   const stateDifference = state.filter(comment => !action.payload.some(value => value.id === comment.id));
@@ -17,3 +21,12 @@ function addComments(state, action) {
     return (a.created_time < b.created_time) ? 1 : (a.created_time > b.created_time) ? -1 : 0;
   });
 }
+
+function containsBlackListedWord(message) {
+  const words = message.split(' ');
+  return words.some(word => blackList.includes(word));
+}
+
+const blackList = [
+  'thor', 'captain america', 'thanos', 'avengers', 'black widow', 'captain', 'cap', 'iron man', 'ironman', 'hawkeye', 'natasha', 'romanov', 'hulk', 'bruce', 'bruce banner', 'banner', 'tony stark', 'stark', 'tony', 'starlord', 'chris pratt', 'pratt', 'ratchet', 'groot', 'drax', 'gamora', 'peter quill', 'quill', 'nebula', 'yondu', 'nova', 'kraglin', 'kraglen', 'vision', 'infinity stone', 'infinity', 'gauntlet', 'asgard', 'asgardian', 'odin', 'loki', 'shield', 'agents of', 'scarlet witch', 'spiderman', 'black panther', 'peter parker', 'peter', 'falcon', 'scarlet', 'wanda', 'maximoff', 'hammer', 'jarvis', 'antman', 'ant-man', 'ant man', 'bucky', 'buckey', 'bucky barnes', 'barnes', 'winter solder', 'soldier', 'wakanda', 'wakanada', 'vibranium', 'valkyrie', 'korg', 'dies', 'kills'
+];
